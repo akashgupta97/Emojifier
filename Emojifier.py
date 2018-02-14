c8eab94e8cf142955d7b075eaaa8985c1a192054
Akash <akash.darknight@gmail.com>
2018-02-14 14:05:00 +0530

    Main script

--- Emojifier.py
+++ Emojifier.py
@@ -10,3 +10,7 @@ X_train, Y_train = read_csv('data/train_emoji.csv')
 X_test, Y_test = read_csv('data/tesss.csv')
 
 maxLen = len(max(X_train, key=len).split())
+
+
+index = 1
+print(X_train[index], label_to_emoji(Y_train[index]))
