523b4a75586935401ce6572ebc95f6b976de1ed5
Akash <akash.darknight@gmail.com>
2018-03-04 14:05:00 +0530

    operations on word vectors

--- Practice.py
+++ Practice.py
@@ -3,3 +3,18 @@ import numpy as np
 from w2v_utils import *
 
 words, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+
+# GRADED FUNCTION: cosine_similarity
+
+def cosine_similarity(u, v):
+    """
+    Cosine similarity reflects the degree of similariy between u and v
+
+    Arguments:
+        u -- a word vector of shape (n,)
+        v -- a word vector of shape (n,)
+
+    Returns:
+        cosine_similarity -- the cosine similarity between u and v defined by the formula above.
+    """
