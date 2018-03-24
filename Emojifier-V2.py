4f802f857ba2b9e61ae44616aa58abd33770a70e
Akash <akash.darknight@gmail.com>
2018-03-24 14:05:00 +0530

    Using LSTM IN Keras

--- Emojifier-V2.py
+++ Emojifier-V2.py
@@ -6,3 +6,20 @@ from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.initializers import glorot_uniform
 np.random.seed(1)
+
+
+# GRADED FUNCTION: sentences_to_indices
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4).
+
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this.
+
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
