270ca8734265ed1154fd26151f585bd4915f6878
Akash <akash.darknight@gmail.com>
2018-03-25 14:05:00 +0530

    Using LSTM IN Keras

--- Emojifier-V2.py
+++ Emojifier-V2.py
@@ -23,3 +23,17 @@ def sentences_to_indices(X, word_to_index, max_len):
     Returns:
     X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
     """
+
+    m = X.shape[0]  # number of training examples
+
+    ### START CODE HERE ###
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m, max_len))
+
+    for i in range(m):  # loop over training examples
+
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = X[i].lower().split()
+
+        # Initialize j to 0
+        j = 0
