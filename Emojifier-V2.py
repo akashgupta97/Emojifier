b8321f0199cab3992d17b6508ca88abf9c866d7f
Akash <akash.darknight@gmail.com>
2018-04-02 14:05:00 +0530

    Using LSTM IN Keras

--- Emojifier-V2.py
+++ Emojifier-V2.py
@@ -79,3 +79,15 @@ def pretrained_embedding_layer(word_to_vec_map, word_to_index):
 
     vocab_len = len(word_to_index) + 1  # adding 1 to fit Keras embedding (requirement)
     emb_dim = word_to_vec_map["cucumber"].shape[0]  # define dimensionality of your GloVe word vectors (= 50)
+
+    ### START CODE HERE ###
+    # Initialize the embedding matrix as a numpy array of zeros of shape (vocab_len, dimensions of word vectors = emb_dim)
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+
+    # Set each row "index" of the embedding matrix to be the word vector representation of the "index"th word of the vocabulary
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+
+    # Define Keras embedding layer with the correct output/input sizes, make it trainable. Use Embedding(...). Make sure to set trainable=False.
+    embedding_layer = Embedding(vocab_len, emb_dim)
+    ### END CODE HERE ###
